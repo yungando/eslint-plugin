7686e2414934f4cfff4cd6562cd652d3d30725b5
James Anderson <yung@ando.ws>
2026-03-30 17:25:23 +0100

    Move dedent regex to module scope

--- src/utils/dedent.js
+++ src/utils/dedent.js
@@ -1,8 +1,10 @@
+const leadingWhitespace = /^ {2,}/gm;
+
 // eslint-disable-next-line import/prefer-default-export
 export const dedent = (strings) => {
   const str = typeof strings === 'string'
     ? strings
     : strings[0];
 
-  return str.replace(/^ {2,}/gm, '').trim();
+  return str.replace(leadingWhitespace, '').trim();
 };
